9e7e09e100626437adff8dcf1bb4cfc52328a3ad
Felipe Assis <169077824+felipeassiss@users.noreply.github.com>
2025-06-07 11:59:25 -0300

    Commit Projeto

--- src/main/java/net/goldi/unicapmod/item/RAScreen.java
+++ src/main/java/net/goldi/unicapmod/item/RAScreen.java
@@ -1,4 +1,4 @@
-package net.goldi.unicapmod.item; // Ou o pacote correto da sua RAScreen.java
+package net.goldi.unicapmod.item;
 
 import net.goldi.unicapmod.UnicapMod;
 import net.minecraft.client.Minecraft;
@@ -9,7 +9,6 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Player;
 
 public class RAScreen extends Screen {
-    // Textura para a foto placeholder do McLovin
     private static final ResourceLocation PLACEHOLDER_PHOTO_TEXTURE =
             ResourceLocation.fromNamespaceAndPath(UnicapMod.MOD_ID, "textures/gui/mclovin_placeholder.png");
 
@@ -55,14 +54,14 @@ public class RAScreen extends Screen {
             guiGraphics.blit(PLACEHOLDER_PHOTO_TEXTURE, photoActualX, photoActualY, 0, 0, PHOTO_WIDTH, PHOTO_HEIGHT, PHOTO_WIDTH, PHOTO_HEIGHT);
         } catch (Exception e) {
             UnicapMod.LOGGER.error("Falha ao carregar PLACEHOLDER_PHOTO_TEXTURE: " + PLACEHOLDER_PHOTO_TEXTURE.toString(), e);
-            guiGraphics.fill(photoActualX, photoActualY, photoActualX + PHOTO_WIDTH, photoActualY + PHOTO_HEIGHT, 0xFFFF0000); // Quadrado vermelho se falhar
+            guiGraphics.fill(photoActualX, photoActualY, photoActualX + PHOTO_WIDTH, photoActualY + PHOTO_HEIGHT, 0xFFFF0000);
             guiGraphics.drawString(this.font, "IMG ERR", photoActualX + 2, photoActualY + 2, 0xFFFFFFFF);
         }
 
         int textX = photoActualX + TEXT_START_X_OFFSET_FROM_PHOTO;
         int textY = guiY + TEXT_Y_OFFSET;
         int lineHeight = this.font.lineHeight + 2;
-        int textColor = 0xFF000000; // Preto
+        int textColor = 0xFF000000;
 
         guiGraphics.drawString(this.font, "NOME:", textX, textY, textColor, false);
         textY += lineHeight;
